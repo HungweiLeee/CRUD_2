fce563198e80905ce5bf3a331680680b5691e28f
plok74122 <plok74122@hotmail.com>
2016-08-12 12:45:57 +0800

    action create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,4 +17,20 @@ class EventsController < ApplicationController
     # 透過Event model 產生一個空的Event物件，透過實例變數（instance variable）傳遞到view中。
     @event = Event.new
   end
+
+  # events/create
+  # 這個action是接收new表單post過來的東西後進行新增資料
+  # !!!! action create並沒有對應的view 新增完成後這個action 是進行轉跳
+  def create
+    # 處理strong parameter
+    # https://ihower.tw/rails4/basic.html 參考 新增資料 章節
+    # @event = Event.new(_____)
+
+    # 透過save的方法存入資料庫
+
+    # 轉跳到show
+    # action show是顯示一筆資料 因此redirect_to 除了告訴rails 要去哪個action外
+    # 還需要帶入:id => @event 告訴rails是哪一筆資料
+
+  end
 end
