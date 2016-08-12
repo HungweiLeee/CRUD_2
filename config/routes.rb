0d4a05482b1a13422bc70741edf95e7536e269a3
plok74122 <plok74122@hotmail.com>
2016-08-12 11:17:16 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 外卡路由
+  # ex: http://localhosts:3000/events/index
+  # 路由會把request丟到controller 是 events , action 是 index 中執行後由該方法產生view
+  match ':controller(/:action(/:id(.:format)))', :via => :all
 end
